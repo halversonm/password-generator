cecc1a7fd475d6307842f3defce1b507609be6f5
mh <maxhalverson@live.com>
2023-05-16 18:33:17 -0500

    created variables for character array and first few prompt/confirm screens

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 // Assignment code here
+var characterLength = window.prompt("How many characters would you like your password to contain?");
+console.log("Character Length " + characterLength);
+
+var specialCharacter = window.confirm("Click OK to confirm including special characters");
+// if (specialCharacter) {}
+  // add special character array to array of options
+
+  var numericCharacter = window.confirm("Click OK to confirm including numeric characters")
+
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+var specialArray = [];
+var uppercaseArray = [];
+var lowercaseArray = [];
+var numericArray = [];
 
 // Write password to the #password input
 function writePassword() {
